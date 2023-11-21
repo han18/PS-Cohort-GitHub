78b8b6efa14ef91d64df922eb1c03c7c56b03267
han18 <han.fadah@gmail.com>
2023-11-21 15:54:53 -0500

    Practice Activity 1:

--- Functions/functionscript.js
+++ Functions/functionscript.js
@@ -45,3 +45,45 @@ add(1, 3); // the position matter
 // arrow
 const results = add(2, 3);
 console.log(results);
+
+// Exercise
+
+function areBothEven(n1, n2) {
+  return !(n1 % 2) && !(n2 % 2);
+}
+
+// I consoled it without catching into a variable
+console.log(areBothEven(4, 6)); // true because both arguments are even number
+
+//Practice Activity 1: Write a Function Declaration
+//Write a function named computeArea using the function declaration approach.
+
+function size(width, height) {
+  theSize = width * height;
+  console.log(
+    `The area of a rectangle with a width of ${width} and a height of ${height} is ${theSize} square units.`
+    // I should have returned it with the return and backticks
+  );
+}
+
+size(3, 6);
+
+// Arguments Array like
+function getDevObject(name) {
+  //   console.log(name);
+  //   console.log(arguments[3]);
+  const skills = [];
+  for (let i = 0; i < arguments; i++) {
+    // adding one by one by looping each argument
+    skills.push(arguments[i]);
+  }
+  return {
+    devName: name,
+    jobSkills: skills, // catch the arguments in here
+  };
+}
+
+// can call mulit
+const getObject = getDevObject("Maria", "Html", "js");
+
+console.log(getObject);
