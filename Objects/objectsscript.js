261be0c76c5f6551150524d9472a74e1a3e3f6cd
han18 <han.fadah@gmail.com>
2023-11-21 07:55:43 -0500

    adding and removing elemnts in an objects

--- Objects/objectsscript.js
+++ Objects/objectsscript.js
@@ -4,11 +4,12 @@ const myCar = {
   color: "Blue",
   hp: 4000,
   isElectric: false,
-  available: true,
+  available: ["xx", " hx", "lk"],
   year: 2023,
   engine: {
     cylinder: 4,
   },
+  color: "Hot pink", // it over rights the pervious key and value
 };
 
 console.log(myCar);
@@ -17,3 +18,25 @@ console.log(myCar["color"]);
 console.log(myCar.isElectric); // dot notation
 console.log(myCar.year);
 console.log(myCar.engine.cylinder); // access within the object
+
+console.log(myCar.available[2]); // for the array
+
+//doing some changes by the dot notation
+myCar.hp = 500;
+console.log(myCar);
+
+myCar.color = "Silver";
+console.log(myCar);
+
+// delete property
+
+delete myCar.isElectric;
+console.log(myCar);
+
+// iterating through an object
+
+for (let key in myCar) {
+  console.log(myCar[key]);
+}
+
+// const myList = [["Chips", 20] ["Candy", 10 ] ["Bread", 5] ["Chocolate", 4] ["Cake", 2] ["Dressing", 3]];
