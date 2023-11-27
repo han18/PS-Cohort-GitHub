62586726e116db0dccbd831c6213a50c3b155f04
han18 <han.fadah@gmail.com>
2023-11-27 09:47:25 -0500

    working on default parameter

--- Data-Collection/newscript.js
+++ Data-Collection/newscript.js
@@ -1,4 +1,12 @@
-console.log("Hi there");
+// working with data collection
+
+// Declare a variable that stores the number of columns in each row of data within the CSV.
+// Instead of hard-coding four columns per row, expand your code to accept any number of columns. This should be calculated dynamically based on the first row of data.
+
+// creating a function to expand the selection of columns
+const stringArray = function (array) {
+  let addArray = array.push([]);
+};
 
 let stringCVS =
   "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
@@ -9,8 +17,8 @@ let splitCVS = stringCVS.split("\n");
 // looping through each row using split method to create an array
 for (let cvs = 0; cvs < splitCVS.length; cvs++) {
   // splitting the row into cells
-  const cells = splitCVS[cvs].split(","); // removing the comma
-  splitCVS.split = [];
+  let cells = splitCVS[cvs].split(","); // removing the comma
+  //   converting the string into an array
 
   // Log each row of data since now it's considered an array reference the index
   console.log(cells[0], cells[1], cells[2], cells[3]);
