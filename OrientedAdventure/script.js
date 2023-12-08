629741f210c45359ec05a5b5a420913a81144376
han18 <han.fadah@gmail.com>
2023-12-08 13:30:18 -0500

    created and connected the files

---
+++ OrientedAdventure/script.js
@@ -0,0 +1,12 @@
+// Lab work 308A.2.1
+
+const adventurer = {
+  name: "Robin",
+  health: 10,
+  inventory: ["sword", "potion", "artifact"],
+};
+console.log(adventurer.inventory[0]);
+
+for (const i of adventurer.inventory) {
+  console.log(i);
+}
