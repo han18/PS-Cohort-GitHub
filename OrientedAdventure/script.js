30bbe5fcdac92201467c91e0f2af5b6f994684e8
han18 <han.fadah@gmail.com>
2023-12-08 13:48:24 -0500

    created nested arrays and objects

--- OrientedAdventure/script.js
+++ OrientedAdventure/script.js
@@ -4,9 +4,23 @@ const adventurer = {
   name: "Robin",
   health: 10,
   inventory: ["sword", "potion", "artifact"],
+  companion: {
+    name: "Leo",
+    type: "Cat",
+    friend: {
+      name: "Frank",
+      type: "Flea",
+    },
+  },
 };
-console.log(adventurer.inventory[0]);
+console.log(adventurer.inventory);
 
+// looping through the inventory using a for of loop
 for (const i of adventurer.inventory) {
-  console.log(i);
+  console.log([i]);
+}
+
+// to loop in the object
+for (const x in adventurer) {
+  console.log(x);
 }
