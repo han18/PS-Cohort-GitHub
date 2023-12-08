edd96fef63904cd329d03eef2078390b27e655d8
han18 <han.fadah@gmail.com>
2023-12-08 14:27:41 -0500

    created a Class object and constructor

--- OrientedAdventure/script.js
+++ OrientedAdventure/script.js
@@ -12,9 +12,31 @@ const adventurer = {
       type: "Flea",
     },
   },
-};
-console.log(adventurer.inventory);
 
+  roll(mod = 0) {
+    const result = Math.floor(Math.random() * 20) + 1 + mod;
+    console.log(`${this.name} rolled a ${result}.`);
+  },
+};
+
+// creating the generic Class to hold repeated properties
+
+class Character {
+  constructor(name, type, health, inventory) {
+    this.name = name;
+    this.type = type;
+    this.health = 100;
+    this.inventory = [];
+  }
+}
+
+// logging the full object
+console.log(adventurer);
+
+// logging the random roll
+adventurer.roll();
+
+//============================
 // looping through the inventory using a for of loop
 for (const i of adventurer.inventory) {
   console.log([i]);
@@ -24,3 +46,4 @@ for (const i of adventurer.inventory) {
 for (const x in adventurer) {
   console.log(x);
 }
+//============================
