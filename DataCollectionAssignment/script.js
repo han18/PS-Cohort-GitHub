a79d0f2467defe6d20d710581dc76d4bcd9c57d8
han18 <han.fadah@gmail.com>
2023-12-19 14:58:30 -0500

    logging out the results

--- DataCollectionAssignment/script.js
+++ DataCollectionAssignment/script.js
@@ -7,3 +7,14 @@ function arrayFormat(array) {
 // creating the variable that holds the array
 let stringCVS =
   "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+
+// using the split method to split the string
+let splitCVS = stringCVS.split("\n");
+
+for (let cvs = 0; cvs < splitCVS.length; cvs++) {
+  // splitting the row into cells
+  let cells = splitCVS[cvs].split(","); // removing the comma
+  //   converting the string into an array
+  // Log each row of data since now it's considered an array reference the index
+  console.log(cells[0], cells[1], cells[2], cells[3]);
+}
