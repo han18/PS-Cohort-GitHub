176d361461ec2fab8893417a58a5f2d2ef000305
han18 <han.fadah@gmail.com>
2023-12-19 14:49:48 -0500

    This is my assignment

---
+++ DataCollectionAssignment/script.js
@@ -0,0 +1,9 @@
+console.log("hey there world");
+
+function arrayFormat(array) {
+  const addArray = array.push([]);
+}
+
+// creating the variable that holds the array
+let stringCVS =
+  "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
